97f9c96131a358f450f2015b2fa4a7a0db1d17df
yuxiang tan <yuxiang.tan@gmail.com>
2015-05-13 10:25:12 -0400

    fucntion to convert F to C

--- src/temp_functions.py
+++ src/temp_functions.py
@@ -3,4 +3,9 @@ def k_to_c(tempr):
 
 def f_to_k(tempr):
 	temp_c=(tempr-32)*5/9+273.15
-reture temp_c
+	reture temp_c
+
+def f_to_c(temp):
+	temp_k = f_to_k(temp)
+	result = k_to_c(temp_k)
+	return result
